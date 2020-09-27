ed921c6c581f5ae61836474cd78a8a499ba508ae
Daniel Schmidt <danielschmidt369@gmail.com>
2020-09-27 11:31:47 +0200

    Auto Ausstempeln beim Shutdown

--- Stechuhr.UI/MainWindow.xaml.cs
+++ Stechuhr.UI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Threading;
 
@@ -11,6 +12,17 @@ namespace Stechuhr.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum CloseReason
+        {
+            EndTask,
+            Logoff,
+            User
+        }
+        /// <summary>
+        /// Must be reset to CloseReason.EndTask if closing is canceled
+        /// </summary>
+        private CloseReason _closeReason;
+
         private System.Windows.Forms.NotifyIcon notifyIcon = null;
 
         private bool AllowClosing = false;
@@ -43,26 +55,27 @@ namespace Stechuhr.UI
                 }
             };
             notifyIcon.Icon = new System.Drawing.Icon("Stechuhr.ico");
-            
-            
+
             Closing += (s, e) =>
             {
-                if (!AllowClosing)
-                {
-                    e.Cancel = true;
-                    Hide();
-                }
-                else
+                if (_closeReason == CloseReason.Logoff || AllowClosing)
                 {
                     if (worktimeProvider.Status == WorktimeStatus.Working)
                     {
                         Stempeln(btnStempeln, null);
                     }
                 }
+                else 
+                {
+                    e.Cancel = true;
+                    _closeReason = CloseReason.EndTask;
+                    Hide();
+                }
             };
 
             Loaded += (s, e) =>
             {
+                ((HwndSource)PresentationSource.FromDependencyObject(this)).AddHook(WndProc);
                 notifyIcon.Visible = true;
             };
 
@@ -124,5 +137,25 @@ namespace Stechuhr.UI
             }
         }
 
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x11:
+                case 0x16:
+                    _closeReason = CloseReason.Logoff;
+                    break;
+
+                case 0x112:
+                    if (((ushort)wParam & 0xfff0) == 0xf060)
+                        _closeReason = CloseReason.User;
+                    break;
+
+                    // CloseReason.EndTask gets a 0x10 windows message which is got by CloseReason.User too,
+                    // so we have no way to identify it,
+                    // except knowing that we did not got any of the specific messages of the other CloseReasons
+            }
+            return IntPtr.Zero;
+        }
     }
 }
